8ca18aec23956f5a5df554f99bc70c1ad5db0fc5
Eisa Alserkal <eisa.alserkal@gmail.com>
2022-02-28 18:22:14 +0400

    Added some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :restaurants, only: [:create, :new, :destroy, :show] do
+    resources :covers, only: [:show, :create, :destroy]
+    resources :foods
+    resources :orders, only: [:index]
+    resources :waiters
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
