d5f663baa9050382e81ee441d7a6818d1c3f31d1
Ahmed Albaiti <ahmedalbaiti1@gmail.com>
2022-03-02 12:23:58 +0400

    basket functionality added in case of adding quantity

--- app/javascript/controllers/order_controller.js
+++ app/javascript/controllers/order_controller.js
@@ -3,10 +3,11 @@ const foodsArray = []
 let price = 0
 export default class extends Controller {
 
-  static targets = ["price", "quantity"]
+  static targets = ["price", "quantity", "modal"]
 
   connect() {
     console.log("connected to order controller")
+    console.log(this.modalTarget)
   }
 
   increase(event) {
@@ -21,12 +22,30 @@ export default class extends Controller {
     if(objIndex !== -1) {
       foodsArray[objIndex].quantity += 1
     } else {
-    foodsArray.push({id: event.target.id, quantity: 1})
+    foodsArray.push({id: event.target.id, quantity: 1, name: event.target.parentElement.querySelector('.name').innerText, description:event.target.parentElement.querySelector('.description').innerText })
 
   }
   console.log(foodsArray)
   this.quantityTarget.innerText = foodsArray.length
   price += parseFloat(event.target.parentElement.querySelector('.price').innerText)
   this.priceTarget.innerText = price
+
+  console.log(event.target.parentElement.querySelector('.name').innerText)
+  let modalHTML = '<div class="container" >'
+  foodsArray.forEach((obj)=>{
+    modalHTML += `  <div class="card-product col-sm-12 m-2 d-flex justify-content-between">
+    <div class="card-product-infos">
+      <h2>${obj.name}</h2>
+      <p>${obj.description}</p>
+    </div>
+      <div class="mr-2">
+        <h2>Quantity</h2>
+        <p class="fs-4 border border-3 rounded-4 p-1"> ${obj.quantity}</p>
+      </div>
+    </div>`
+  })
+  modalHTML += ` </div>`
+  this.modalTarget.innerHTML = modalHTML
 }
+
 }
