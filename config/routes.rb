e10b6c7c7cca08d5b8daea6b4c61e9c75f3d9b17
Eisa Alserkal <eisa.alserkal@gmail.com>
2022-03-02 15:00:44 +0400

    Added dashboard page with pagination, front-end is still needing work on

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
     resources :waiters
     resources :receipts, only: [:index]
   end
+  get "/dashboard", to: "restaurants#dashboard"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
