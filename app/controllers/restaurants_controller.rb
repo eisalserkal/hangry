ac30936457a14fd6fbb763e6f0862d65c6ce943c
Mohammad Alserkal <mdalserkal47@gmail.com>
2022-03-03 18:18:05 +0400

    navbar slight edit in style and restaurants controller create method redirects to dashboard

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -24,7 +24,7 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new(restaurant_params)
     @restaurant.user = current_user
     if @restaurant.save
-      redirect_to restaurant_path(@restaurant)
+      redirect_to dashboard_path(@restaurant)
     else
       render :new
     end
