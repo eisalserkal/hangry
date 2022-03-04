03f990643c5b82577f2fdebd76c79af286181436
Ahmed Albaiti <ahmedalbaiti1@gmail.com>
2022-03-04 09:49:31 +0400

    added delete option for requests

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
     resources :requests, only: [:create]
   end
   resources :receipts, only: [:show]
+  resources :requests, only: [:destroy]
   get "/dashboard", to: "restaurants#dashboard"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
