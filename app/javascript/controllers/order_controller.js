8e79a71a9aa08184f8fcd793fe18c2d5ad264a29
Mohammad Alserkal <mdalserkal47@gmail.com>
2022-03-10 14:10:52 +0400

    final touches

--- app/javascript/controllers/order_controller.js
+++ app/javascript/controllers/order_controller.js
@@ -33,9 +33,9 @@ export default class extends Controller {
     })
 
     console.log(event.target.parentElement.querySelector('.name1').innerText)
-    let modalHTML = '<div class="container" >'
+    let modalHTML = '<div>'
     this.foodsArray.forEach((obj) => {
-      modalHTML += `  <div class="card-product col-sm-12 m-2 d-flex justify-content-between">
+      modalHTML += `  <div class="card-product col-sm-12 d-flex justify-content-between">
     <div class="card-product-infos">
       <h2>${obj.name}</h2>
       <p>${obj.description}</p>
